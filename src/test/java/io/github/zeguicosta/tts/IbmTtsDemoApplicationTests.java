cca477f9a2418e6bb41db349422568f38b6577c7
Guilherme <joseguilhermecosta@gmail.com>
2025-10-09 20:22:51 -0300

    chore: projeto inicial com dependências e estrutura base

---
+++ src/test/java/io/github/zeguicosta/tts/IbmTtsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zeguicosta.tts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmTtsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
